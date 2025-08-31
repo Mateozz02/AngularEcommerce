9e549017d239034ca70452c9576cef7d4f5e57be
Mateozz02 <ibanezmateo03@gmail.com>
2025-08-31 13:24:00 -0300

    LP Save

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,7 +1,21 @@
 import { Component, Injectable } from "@angular/core";
-
+import {environment} from "../env"
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({providedIn : "root"})
 export class AuthService{
+    private apiUrl = `${environment.apiUrl}`;
     
+
+    constructor(private http : HttpClient){}
+    
+     login(credencial : {username : string,password : string}){
+        var url = `${this.apiUrl}/users`;
+        return this.http.post(url,credencial).subscribe({
+            next : res => {
+
+            }
+        })
+    }
+
 }
